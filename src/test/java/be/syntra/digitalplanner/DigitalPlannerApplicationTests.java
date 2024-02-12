dbec0e72f5198f5fc73ab27f165f1b0df10dd132
audreyderks <114101247+audreyderks@users.noreply.github.com>
2024-02-12 21:07:02 +0100

    initial setup

---
+++ src/test/java/be/syntra/digitalplanner/DigitalPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.syntra.digitalplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
