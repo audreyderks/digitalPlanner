0aa7ee0afd355cf291d67a92b480b96d7c08b6c8
Soner Demircan <S.onerr@hotmail.com>
2024-02-14 23:18:49 +0100

    * Config directory aangemaakt -- WebConfig class aangemaakt die ervoor zorgt dat static folder wordt herkend * Homescreencontroller aangemaakt -- GET request voor stylesheetHomescreen.css in te laden * homescreen.html -- sidebar aangemaakt met aangepaste css stijl

--- src/main/java/be/syntra/digitalplanner/DigitalPlannerApplication.java
+++ src/main/java/be/syntra/digitalplanner/DigitalPlannerApplication.java
@@ -2,8 +2,10 @@ package be.syntra.digitalplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+//@EnableWebMvc
 public class DigitalPlannerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/be/syntra/digitalplanner/config/WebConfig.java
@@ -0,0 +1,17 @@
+package be.syntra.digitalplanner.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// static folder created manually --> not recognized by Spring Boot
+// Config to explicitly designate static folder as the location for static resources
+// WebMvcConfigurer implementation --> override addResourceHandlers method
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
